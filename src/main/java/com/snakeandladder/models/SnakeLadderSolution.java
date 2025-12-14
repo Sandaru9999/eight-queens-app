6d11ca436ca2b38c33c6f509a0e465a1eb44f59b
SanduHans <233099904+sanduni-hansika@users.noreply.github.com>
2025-12-14 20:10:40 +0530

    Added Snake and Ladder game implementation

--- src/main/java/com/snakeandladder/models/SnakeLadderSolution.java
+++ src/main/java/com/snakeandladder/models/SnakeLadderSolution.java
@@ -1,35 +1,82 @@
+
 package com.snakeandladder.models;
+
+import java.util.List;
 
 import java.util.Map;
 
+
 public class SnakeLadderSolution {
 
-    private String playerName;
-    private int boardSize;
-    private Map<Integer, Integer> snakes;
-    private Map<Integer, Integer> ladders;
-    private int playerAnswer;
-    private int correctThrows;
-    private long timeTakenMs;
+private String playerName;
 
-    public String getPlayerName() { return playerName; }
-    public void setPlayerName(String playerName) { this.playerName = playerName; }
+private int boardSize;
 
-    public int getBoardSize() { return boardSize; }
-    public void setBoardSize(int boardSize) { this.boardSize = boardSize; }
+private int correctMinThrows;
 
-    public Map<Integer, Integer> getSnakes() { return snakes; }
-    public void setSnakes(Map<Integer, Integer> snakes) { this.snakes = snakes; }
+private int playerAnswer;
 
-    public Map<Integer, Integer> getLadders() { return ladders; }
-    public void setLadders(Map<Integer, Integer> ladders) { this.ladders = ladders; }
+private long algo1TimeNanos; 
 
-    public int getPlayerAnswer() { return playerAnswer; }
-    public void setPlayerAnswer(int playerAnswer) { this.playerAnswer = playerAnswer; }
+private long algo2TimeNanos; 
 
-    public int getCorrectThrows() { return correctThrows; }
-    public void setCorrectThrows(int correctThrows) { this.correctThrows = correctThrows; }
+private Map<Integer, Integer> snakes;
 
-    public long getTimeTakenMs() { return timeTakenMs; }
-    public void setTimeTakenMs(long timeTakenMs) { this.timeTakenMs = timeTakenMs; }
+private Map<Integer, Integer> ladders;
+
+private List<Integer> optimalPath;
+
+
+public void setPlayerName(String playerName) { this.playerName = playerName; }
+
+public String getPlayerName() { return playerName; }
+
+
+
+public void setBoardSize(int boardSize) { this.boardSize = boardSize; }
+
+public int getBoardSize() { return boardSize; }
+
+
+
+public void setCorrectMinThrows(int correctMinThrows) { this.correctMinThrows = correctMinThrows; }
+
+public int getCorrectMinThrows() { return correctMinThrows; }
+
+
+
+public void setPlayerAnswer(int playerAnswer) { this.playerAnswer = playerAnswer; }
+
+public int getPlayerAnswer() { return playerAnswer; }
+
+
+
+public void setAlgo1TimeNanos(long algo1TimeNanos) { this.algo1TimeNanos = algo1TimeNanos; }
+
+public long getAlgo1TimeNanos() { return algo1TimeNanos; }
+
+
+
+public void setAlgo2TimeNanos(long algo2TimeNanos) { this.algo2TimeNanos = algo2TimeNanos; }
+
+public long getAlgo2TimeNanos() { return algo2TimeNanos; }
+
+
+
+public void setSnakes(Map<Integer, Integer> snakes) { this.snakes = snakes; }
+
+public Map<Integer, Integer> getSnakes() { return snakes; }
+
+
+
+public void setLadders(Map<Integer, Integer> ladders) { this.ladders = ladders; }
+
+public Map<Integer, Integer> getLadders() { return ladders; }
+
+
+
+public List<Integer> getOptimalPath() { return optimalPath; }
+
+public void setOptimalPath(List<Integer> optimalPath) { this.optimalPath = optimalPath; }
+
 }
